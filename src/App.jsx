1d61c186a63fe51157101874b098f8b63468e02d
Hunter Andersen <andersen.hunter42@gmail.com>
2022-06-29 10:43:19 -0700

    First Pass through the exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {Component} from 'react';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+
+    this.state = {
+      text: "init string",
+      hasLoaded: false,
+    };
   }
 
-  render(){
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      ...{
+        hasLoaded: true,
+      },
+    });
+  }
+
+  handleClick = () => {
+    let tempObj = {
+      hasLoaded: !this.state.hasLoaded,
+    };
+
+    this.setState({
+      ...this.state,
+      ...tempObj,
+    });
+  };
+
+  handleInput = (event) => {
+    let tempObj = {
+      text: event.target.value,
+    };
+
+    this.setState({
+      ...this.state,
+      ...tempObj,
+    });
+  };
+
+  render() {
     return (
       <div className="App">
+        <Navbar></Navbar>
         <h1>Welcome to React!</h1>
         <hr />
+        {this.state.hasLoaded ? (
+          <div>
+            <input
+              type="text"
+              name="inputMain"
+              id="inputMain"
+              placeholder={this.state.text}
+              onChange={this.handleInput}
+            />
+            <button onClick={this.handleClick}>Toggle Me</button>
+          </div>
+        ) : (
+          <button onClick={this.handleClick}>Second Toggle</button>
+        )}
       </div>
     );
   }
